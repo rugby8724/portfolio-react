8bdc52fe7964652578dae3e8d4f35009b326cbea
Jeremiah <jeremiah.d.wise@gmail.com>
2022-09-14 16:23:14 -0500

    update portolio

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-green-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+          </div>
+          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Hi. I'm Jeremiah, a CPA looking to make a career change into the world of programing </p>
+            </div>
+            <div>
+              <p>I am passionate about learning new things and a few years ago a friend 
+                introduced me to programming. After building small projects in 2021
+                I decided to dedicate more time to programming and really enjoyed the process. 
+                During that time my mentor encourage me to purse a career in software developement. 
+                I took his advice and started the Digital Crafts course in 2022. 
+                Digital Crafts has been an amazing tool to help get me out of my comfort zone
+                and push me from just being a hobby developer to having the skills needed for a career
+                as a developer. I know there is still a lot to learn, and I'm looking forward to growing 
+                as a developer with a great company. 
+              </p>  
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Work.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import WorkImg from '../assets/workImg.jpeg';
+import realEstate from '../assets/realestate.jpg';
+import farkle from '../assets/farkle.png'
+import digitalBuilding from '../assets/digitalBuilding.png'
+import canoe from '../assets/canoe.png'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-green-600'>
+            Projects
+          </p>
+          <p className='py-6'>// Check out some of my recent projects</p>
+        </div>
+
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${digitalBuilding})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                MERN Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${canoe})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${farkle})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            // style={{ backgroundImage: `url(${realEstate})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            <div >
+              <span className='text-base font-bold text-white tracking-wider'>
+                Digital Buildings helps a company track their commerical clients
+                hvac systems. <br/>
+                Another student and myself built this project using the 
+                MERN stack. My main responiblities were managing the backend, database,
+                and global state.
+              </span>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            <div >
+              <span className='text-base font-bold text-white tracking-wider'>
+                Canoe is a traveling site that lets customers see future flights and track current flights
+                This was a group project built by two other students and myself. 
+                My main resposiblites were fixing bugs, refactoring code, and adding a firebase login.
+              </span>
+            </div>
+          </div>
+          <div
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            <div>
+              <span className='text-base font-bold text-white tracking-wider'>
+                Farkle is a lite-version of the game farkle. 
+                This was a solo project. I built farkle using react and redux toolkit
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
