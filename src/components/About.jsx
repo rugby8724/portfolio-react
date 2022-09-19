e4374075a525be4f55409073989691a61f2282f1
Jeremiah <jeremiah.d.wise@gmail.com>
2022-09-18 19:23:17 -0500

    fix grammer in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,15 +16,7 @@ const About = () => {
               <p>Hi. I'm Jeremiah, a CPA looking to make a career change into the world of programing </p>
             </div>
             <div>
-              <p>I am passionate about learning new things and a few years ago a friend 
-                introduced me to programming. After building small projects in 2021
-                I decided to dedicate more time to programming and really enjoyed the process. 
-                During that time my mentor encourage me to purse a career in software developement. 
-                I took his advice and started the Digital Crafts course in 2022. 
-                Digital Crafts has been an amazing tool to help get me out of my comfort zone
-                and push me from just being a hobby developer to having the skills needed for a career
-                as a developer. I know there is still a lot to learn, and I'm looking forward to growing 
-                as a developer with a great company. 
+              <p>I am passionate about learning new things and a few years ago a friend introduced me to programming. After building small projects in 2021 I decided to dedicate more time to programming and really enjoyed the process. During that time my mentor encouraged me to pursue a career in software development. I took his advice and started the Digital Crafts course in 2022. Digital Crafts has been an amazing tool to help get me out of my comfort zone and push me from just being a hobby developer to having the skills needed for a career as a developer. I know there is still a lot to learn, and I'm looking forward to growing as a developer with a great company.
               </p>  
             </div>
           </div>
